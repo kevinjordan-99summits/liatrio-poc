208f92b8169d6d5c64e226cace58e7e75872f66c
Kevin Jordan <kevin@99summits.com>
2024-01-25 17:44:56 -0600

    Add default route for API.

---
+++ src/LiatrioPoC.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiatrioPoC.Api.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            return Ok("I am a NOT teapot.");
+        }
+    }
+}
