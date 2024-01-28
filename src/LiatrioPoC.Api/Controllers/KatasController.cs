3e16442b91bba1b618846ce2e734ca1db91d430b
Kevin Jordan <kevin@99summits.com>
2024-01-28 15:16:19 -0600

    Enable swagger for all environments.

--- src/LiatrioPoC.Api/Controllers/KatasController.cs
+++ src/LiatrioPoC.Api/Controllers/KatasController.cs
@@ -1,4 +1,5 @@
 ﻿using LiatrioPoC.Api.Models.Katas;
+using LiatrioPoC.Core.Entities;
 using LiatrioPoC.Core.Repositories;
 using LiatrioPoC.Core.Services.Katas;
 using Microsoft.AspNetCore.Mvc;
@@ -22,21 +23,41 @@ namespace LiatrioPoC.Api.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Get all katas
+        /// </summary>
+        /// <response code="200">Returns a list of all katas</response>
         [HttpGet("/katas")]
+        [ProducesResponseType(typeof(IEnumerable<Kata>), 200)]
+        [Produces("application/json")]
         public IActionResult List()
         {
             var katas = new ListKatasQuery(_kataRepository).List();
             return Ok(katas);
         }
 
+        /// <summary>
+        /// Get all katas underneath a category
+        /// </summary>
+        /// <param name="categoryId">The unique identifier for the category the katas are under</param>
+        /// <response code="200">Returns a list of katas under a category</response>
         [HttpGet("/categories/{categoryId:guid}/katas")]
+        [ProducesResponseType(typeof(IEnumerable<Kata>), 200)]
+        [Produces("application/json")]
         public IActionResult GetByCategory([FromRoute] Guid categoryId)
         {
             var katas = new GetKatasByCategoryQuery(_kataRepository).Get(categoryId);
             return Ok(katas);
         }
 
+        /// <summary>
+        /// Create a new kata
+        /// </summary>
+        /// <response code="200">The kata that was created</response>
+        /// <response code="400">Bad parameters</response>
         [HttpPost("/katas")]
+        [ProducesResponseType(typeof(Kata), 200)]
+        [Produces("application/json")]
         public IActionResult Post([FromBody]KatasPostModel model)
         {
             if (!ModelState.IsValid)
--- src/LiatrioPoC.Api/Models/Katas/KatasPostModel.cs
+++ src/LiatrioPoC.Api/Models/Katas/KatasPostModel.cs
@@ -4,13 +4,22 @@ namespace LiatrioPoC.Api.Models.Katas
 {
     public class KatasPostModel
     {
+        /// <summary>
+        /// The name of the kata
+        /// </summary>
         [Required]
         [MaxLength(100)]
         public required string Name { get; set; }
 
+        /// <summary>
+        /// The unique identifier for the category the kata should be under
+        /// </summary>
         [Required]
         public Guid CategoryId { get; set; }
 
+        /// <summary>
+        /// The description of the kata
+        /// </summary>
         [MaxLength(1000)]
         public string? Description { get; set; }
     }
