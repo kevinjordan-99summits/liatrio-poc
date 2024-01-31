fad616af6f32ffbb5a914cea13693368374a639d
Kevin Jordan <kevin@99summits.com>
2024-01-31 11:30:03 -0600

    Create insecure page to test Snyk.

---
+++ src/LiatrioPoC.Web/Controllers/InsecureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiatrioPoC.Web.Controllers
+{
+    public class InsecureController : Controller
+    {
+        [Route("/insecure/xss")]
+        public IActionResult Xss()
+        {
+            return View();
+        }
+    }
+}
