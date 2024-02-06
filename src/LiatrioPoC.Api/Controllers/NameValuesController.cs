e7dd193b802d668f84b218ddc825738504196e38
Kevin Jordan <kevin@99summits.com>
2024-02-06 09:54:11 -0600

    Actually set a value as secret. Duh!

--- src/LiatrioPoC.Api/Controllers/NameValuesController.cs
+++ src/LiatrioPoC.Api/Controllers/NameValuesController.cs
@@ -18,7 +18,7 @@ namespace LiatrioPoC.Api.Controllers
             {
                 new NameValueGetModel{ Id = Guid.NewGuid(), Name = "App Name", Value = "My Site" },
                 new NameValueGetModel{ Id = Guid.NewGuid(), Name = "Environment", Value = "DEV" },
-                new NameValueGetModel{ Id = Guid.NewGuid(), Name = "SecretPassword", Value = "DontLookAtMe!" },
+                new NameValueGetModel{ Id = Guid.NewGuid(), Name = "SecretPassword", Value = "DontLookAtMe!", IsSecret = true },
                 new NameValueGetModel{ Id = Guid.NewGuid(), Name = "BaseUrl", Value = "https://dev.mysite.com" }
             });
         }
