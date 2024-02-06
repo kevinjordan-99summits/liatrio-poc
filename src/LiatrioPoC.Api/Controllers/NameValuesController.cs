9c4786f336a0060f24f02bbfc31a4bd390ecc3df
Kevin Jordan <kevin@99summits.com>
2024-02-06 09:04:19 -0600

    Add name value controller for testing.

---
+++ src/LiatrioPoC.Api/Controllers/NameValuesController.cs
@@ -0,0 +1,25 @@
+﻿using LiatrioPoC.Api.Models.NameValues;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiatrioPoC.Api.Controllers
+{
+    [ApiController]
+    public class NameValuesController : ControllerBase
+    {
+        /// <summary>
+        /// Get a set of name value pairs for testing random things
+        /// </summary>
+        [HttpGet("/key-values")]
+        [ProducesResponseType(typeof(IEnumerable<NameValueGetModel>), 200)]
+        [Produces("application/json")]
+        public IActionResult List()
+        {
+            return Ok(new List<NameValueGetModel>
+            {
+                new NameValueGetModel{ Id = Guid.NewGuid(), Name = "App Name", Value = "My Site" },
+                new NameValueGetModel{ Id = Guid.NewGuid(), Name = "Environment", Value = "DEV" },
+                new NameValueGetModel{ Id = Guid.NewGuid(), Name = "BaseUrl", Value = "https://dev.mysite.com" }
+            });
+        }
+    }
+}
---
+++ src/LiatrioPoC.Api/Models/NameValues/NameValueGetModel.cs
@@ -0,0 +1,20 @@
+﻿namespace LiatrioPoC.Api.Models.NameValues
+{
+    public class NameValueGetModel
+    {
+        /// <summary>
+        /// ID of the setting
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Name of the setting
+        /// </summary>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Value stored in the setting
+        /// </summary>
+        public string? Value{ get; set; }
+    }
+}
